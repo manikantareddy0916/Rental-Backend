fe9e8daf44e028863c865ec2c16a61f802d82928
manikanta <mkr@dct.com>
2025-01-25 19:45:19 +0530

    atlas

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require ('mongoose')
 const configureDB = async()=>{
+    console.log('1')
     try{
         const url = process.env.DB_URL || 'mongodb://127.0.0.1:27017'
         const dbName = process.env.DB_NAME || 'myrental'
+        console.log('2',url,dbName)
         const db = await mongoose.connect(`${url}/${dbName}`)
         console.log('connected to db')
     }
